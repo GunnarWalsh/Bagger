c73571ed084e032ce6299b3e9a8dc6dd14297180
GunnarWalsh <104541337+GunnarWalsh@users.noreply.github.com>
2023-04-25 11:42:54 -0400

    First commit

---
+++ bagger/Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bagger.Data;
+using bagger.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace bagger.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ProductController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: /<cßontroller>/
+        public IActionResult Index()
+        {
+            var products = _dbContext.Products.ToList();
+            return View(products);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _dbContext.Products.Add(product);
+            _dbContext.SaveChanges(); // --> Error Solution: dotnet ef migrations add [migration name] -> dotnet ef database update
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            var item = _dbContext.Products.Find(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Product product)
+        {
+            if(id != product.Id)
+            {
+                return NotFound();
+            }
+            if(ModelState.IsValid)
+            {
+                _dbContext.Update(product);
+                _dbContext.SaveChanges();
+            }
+            return View(product);
+        }
+        public IActionResult Delete(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Remove(product);
+                _dbContext.SaveChanges();
+            }
+            return Redirect("/Product");
+            
+        }
+    }
+}
+
---
+++ bagger/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace bagger.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public int? Count { get; set; }
+		public string? UoM { get; set; }
+
+	}
+}
+
